fc4a7300ec1dbc03b80ae11a470cd0720ef889b5
adlardco <corin.adlard22@gmail.com>
2022-03-11 18:25:58 +0000

    work

--- app.js
+++ app.js
@@ -4,8 +4,8 @@ class App {
 
   constructor(doc) {
     this.letter = index => doc.getElementById('' + index);
-    this.word = 'review';
-    const guess = 'dloebu';
+    this.word = 'before';
+    const guess = 'obefer';
     this.selectedIndex = -1;
 
     for (let index = 0; index < guess.length; index++) {
