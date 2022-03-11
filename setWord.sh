41d2c5a962c3309e5aed86d6f50804d5f185a896
adlardco <corin.adlard22@gmail.com>
2022-03-11 18:09:09 +0000

    work

--- setWord.sh
+++ setWord.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-INDEX=index.html
+APP_FILE=app.js
 WORDS=words.txt
 NUM_WORDS=$(wc -l ${WORDS} | cut -d" " -f1)
 WORD_INDEX=$(expr ${RANDOM} % ${NUM_WORDS})
 WORD=$(head -${WORD_INDEX} ${WORDS} | tail -1)
 WORD=$(echo ${WORD} | sed "s/[^a-zA-Z]//g" | sed -e 's/\(.*\)/\L\1/')
 SCRAMBLED=$(echo ${WORD} | fold -w1 | shuf | tr -d '\n')
-sed -i "s/this.w=\x27[a-zA-Z]*\x27/this.w='${WORD}'/" ${INDEX}
-sed -i "s/let g=\x27[a-zA-Z]*\x27/let g='${SCRAMBLED}'/" ${INDEX}
+sed -i "s/this.word[ ]*=[ ]*\x27[a-zA-Z]*\x27/this.word = '${WORD}'/" ${APP_FILE}
+sed -i "s/[ ]+guess[ ]*=[ ]*\x27[a-zA-Z]*\x27/ guess = '${SCRAMBLED}'/" ${APP_FILE}
