95bd6b8a885fe9e30c50ea2d01657e0f1d74124a
adlardco <corin.adlard22@gmail.com>
2022-03-11 18:24:11 +0000

    work

--- setWord.sh
+++ setWord.sh
@@ -7,4 +7,4 @@ WORD=$(head -${WORD_INDEX} ${WORDS} | tail -1)
 WORD=$(echo ${WORD} | sed "s/[^a-zA-Z]//g" | sed -e 's/\(.*\)/\L\1/')
 SCRAMBLED=$(echo ${WORD} | fold -w1 | shuf | tr -d '\n')
 sed -i "s/this.word[ ]*=[ ]*\x27[a-zA-Z]*\x27/this.word = '${WORD}'/" ${APP_FILE}
-sed -i "s/[ ]+guess[ ]*=[ ]*\x27[a-zA-Z]*\x27/ guess = '${SCRAMBLED}'/" ${APP_FILE}
+sed -i "s/const guess[ ]*=[ ]*\x27[a-zA-Z]*\x27/const guess = '${SCRAMBLED}'/" ${APP_FILE}
