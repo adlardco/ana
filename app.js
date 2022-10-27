9f9df75b23be17167cf5978427c020d8da98fc62
adlardco <corin.adlard22@gmail.com>
2022-10-27 00:05:37 +0000

    Set word

--- app.js
+++ app.js
@@ -4,8 +4,8 @@ class App {
 
   constructor(doc) {
     this.letter = index => doc.getElementById('' + index);
-    this.word = 'advise';
-    const guess = 'edvasi';
+    this.word = 'future';
+    const guess = 'uefrut';
     this.selectedIndex = -1;
 
     for (let index = 0; index < guess.length; index++) {
